ecdbaf110d062f088e55c9b8a76668ac057fa4e8
sieun-Woo <116184724+sieun-Woo@users.noreply.github.com>
2023-02-07 10:58:20 +0900

    Create : Project

---
+++ src/main/java/com/sparta/hotbody/HotBodyApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.hotbody;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotBodyApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HotBodyApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/sparta/hotbody/HotBodyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.hotbody;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotBodyApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}

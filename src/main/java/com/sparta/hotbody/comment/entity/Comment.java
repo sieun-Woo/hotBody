41e65e0a604bdaca6c7a73eaa241df97b0f65d98
Park SangUk <psw1375@gmail.com>
2023-02-09 16:13:42 +0900

    Refactor: import CascadeType

--- src/main/java/com/sparta/hotbody/comment/entity/Comment.java
+++ src/main/java/com/sparta/hotbody/comment/entity/Comment.java
@@ -7,6 +7,7 @@ import com.sparta.hotbody.post.entity.Post;
 import com.sparta.hotbody.user.entity.User;
 import java.util.ArrayList;
 import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;

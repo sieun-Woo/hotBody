128382d5ec181dbf3c569fe8e420bb2f7030a5a2
Park SangUk <psw1375@gmail.com>
2023-02-10 11:33:17 +0900

    Refactor: 게시글, 댓글 좋아요 연관 관계 수정

--- src/main/java/com/sparta/hotbody/post/entity/PostLike.java
+++ src/main/java/com/sparta/hotbody/post/entity/PostLike.java
@@ -1,5 +1,6 @@
 package com.sparta.hotbody.post.entity;
 
+import com.sparta.hotbody.user.entity.User;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
@@ -36,15 +37,25 @@ public class PostLike {
    * 연관관계 - Foreign Key 값을 따로 컬럼으로 정의하지 않고 연관 관계로 정의합니다.
    */
 
+  // 게시글 좋아요와 게시글의 연관 관계
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "user_id")
+  private Post user;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "post_id")
+  private Post post;
+
+
+
+
+
+
 
   /**
    * 연관관계 편의 메소드 - 반대쪽에는 연관관계 편의 메소드가 없도록 주의합니다.
    */
 
-  // 게시글 좋아요와 게시글의 연관 관계
-  @ManyToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "post_id")
-  private Post post;
 
   /**
    * 서비스 메소드 - 외부에서 엔티티를 수정할 메소드를 정의합니다. (단일 책임을 가지도록 주의합니다.)

1cb1b181df9d45060540d9be13feb1f27f58cb8f
milnil0228 <milnil0228@gmail.com>
2023-02-13 19:01:36 +0900

    Fix: 이닛데이터 게시글 추가

--- src/main/java/com/sparta/hotbody/common/InitData.java
+++ src/main/java/com/sparta/hotbody/common/InitData.java
@@ -1,5 +1,7 @@
 package com.sparta.hotbody.common;
 
+import com.sparta.hotbody.post.entity.Post;
+import com.sparta.hotbody.post.repository.PostRepository;
 import com.sparta.hotbody.user.entity.User;
 import com.sparta.hotbody.user.entity.UserRole;
 import com.sparta.hotbody.user.repository.UserRepository;
@@ -16,6 +18,7 @@ public class InitData implements ApplicationRunner {
 
   private final PasswordEncoder passwordEncoder;
   private final UserRepository userRepository;
+  private final PostRepository postRepository;
 
   @Override
   public void run(ApplicationArguments args){
@@ -30,5 +33,17 @@ public class InitData implements ApplicationRunner {
     userRepository.save(user2);
     userRepository.save(user3);
     userRepository.save(user4);
+    Post post1 = new Post("첫번째 게시글", "첫번째 글을 쓰다니! 영광입니다.", user1);
+    Post post2 = new Post("두번째 게시글", "기분이 좋아 하나 더 씁니다!", user1);
+    Post post3 = new Post("세번째 게시글", "세번째는 뺏기지 않았습니다.", user2);
+    Post post4 = new Post("네번째 게시글", "네째는 뺏기지 않았습니다.", user1);
+    Post post5 = new Post("다섯번째 게시글", "다섯번째는 뺏기지 않았습니다.", user2);
+    Post post6 = new Post("여섯번째 게시글", "여섯번째는 뺏기지 않았습니다.", user1);
+    postRepository.save(post1);
+    postRepository.save(post2);
+    postRepository.save(post3);
+    postRepository.save(post4);
+    postRepository.save(post5);
+    postRepository.save(post6);
   }
 }

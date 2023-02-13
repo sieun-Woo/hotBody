9b0959530819ea601662ae7312f5065384650330
milnil0228 <milnil0228@gmail.com>
2023-02-13 17:24:16 +0900

    Fix: 페이저블 관련 오류 수정

--- src/main/java/com/sparta/hotbody/common/page/PageDto.java
+++ src/main/java/com/sparta/hotbody/common/page/PageDto.java
@@ -1,15 +1,23 @@
 package com.sparta.hotbody.common.page;
 
 import javax.validation.constraints.Positive;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 
+@Getter
+@NoArgsConstructor
 public class PageDto {
   @Positive
-  private Integer currentPage;
+  private int currentPage;
 //  private Integer size;
 //  private String sortBy;
+
+  public PageDto(int currentPage) {
+    this.currentPage = currentPage;
+  }
 
   public Pageable toPageable() {
     return PageRequest.of(currentPage-1, 5, Sort.by("id").descending());

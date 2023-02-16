7592c6cd3a9da49988e719ee3eec140e8f85ce88
sieun-Woo <116184724+sieun-Woo@users.noreply.github.com>
2023-02-16 18:14:32 +0900

    Update JasyptConfig.java

--- src/main/java/com/sparta/hotbody/common/config/JasyptConfig.java
+++ src/main/java/com/sparta/hotbody/common/config/JasyptConfig.java
@@ -20,7 +20,6 @@ public class JasyptConfig {
     PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
     SimpleStringPBEConfig config = new SimpleStringPBEConfig();
 
-    
     config.setPassword(encryptKey);
     config.setAlgorithm("PBEWithMD5AndDES");
     config.setKeyObtentionIterations("1000");

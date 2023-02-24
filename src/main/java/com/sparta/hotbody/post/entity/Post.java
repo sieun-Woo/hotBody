504a149deceb8862eaacce8020a1710a8dc9d627
Song Jieun <jieuns2max@gmail.com>
2023-02-24 16:46:10 +0900

    FEAT : 프론트 코멘트 구현을 위해서 수정

--- src/main/java/com/sparta/hotbody/post/entity/Post.java
+++ src/main/java/com/sparta/hotbody/post/entity/Post.java
@@ -1,5 +1,6 @@
 package com.sparta.hotbody.post.entity;
 
+import com.sparta.hotbody.comment.dto.CommentRequestDto;
 import com.sparta.hotbody.comment.entity.Comment;
 import com.sparta.hotbody.common.TimeStamp;
 import com.sparta.hotbody.post.dto.PostModifyRequestDto;
@@ -17,6 +18,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.OrderBy;
 import javax.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -50,6 +52,10 @@ public class Post extends TimeStamp {
   private String image;
   @Column(nullable = false)
   private int likes = 0;
+
+  @OrderBy(value = "modifiedAt desc")
+  @OneToMany(fetch = FetchType.LAZY,cascade = CascadeType.REMOVE)
+  private List<Comment> comments = new ArrayList<>();
 
   /**
    * 생성자 - 약속된 형태로만 생성가능하도록 합니다.
@@ -116,4 +122,9 @@ public class Post extends TimeStamp {
   public void updateImage(String resourcePath) {
     this.image = resourcePath;
   }
+
+  public void addCommentList(Comment comment){
+    this.comments.add(comment);
+  }
 }
+

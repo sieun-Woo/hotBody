605df75a27343300394e97f2ddc200d25ac78a47
sieun-Woo <116184724+sieun-Woo@users.noreply.github.com>
2023-02-26 02:51:27 +0900

    fix : 페이징 내림차순으로 정렬

--- src/main/java/com/sparta/hotbody/post/entity/Post.java
+++ src/main/java/com/sparta/hotbody/post/entity/Post.java
@@ -6,11 +6,14 @@ import com.sparta.hotbody.common.TimeStamp;
 import com.sparta.hotbody.post.dto.PostModifyRequestDto;
 import com.sparta.hotbody.post.dto.PostRequestDto;
 import com.sparta.hotbody.user.entity.User;
+import com.sparta.hotbody.user.entity.UserRole;
 import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -52,13 +55,13 @@ public class Post extends TimeStamp {
   private String image;
   @Column(nullable = false)
   private int likes = 0;
-  @Column
-  private String category;
+  @Column(nullable = false)
+  @Enumerated(value = EnumType.STRING)
+  private PostCategory category;
 
   /**
    * 연관관계 - Foreign Key 값을 따로 컬럼으로 정의하지 않고 연관 관계로 정의합니다.
    */
-
   // 게시글과 유저의 연관 관계(N : 1)
   @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "user_id")
@@ -73,7 +76,6 @@ public class Post extends TimeStamp {
   @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
   private List<PostLike> postLikeList = new ArrayList<>();
 
-
   /**
    * 생성자 - 약속된 형태로만 생성가능하도록 합니다.
    */
@@ -83,9 +85,11 @@ public class Post extends TimeStamp {
     this.nickname = user.getNickname();
     this.title = postRequestDto.getTitle();
     this.content = postRequestDto.getContent();
+    this.category = PostCategory.valueOf(postRequestDto.getCategory());
   }
 
-  public Post(String title, String content, User user) {
+  public Post(String title, String content, User user, PostCategory category) {
+    this.category = category;
     this.user = user;
     this.nickname = user.getNickname();
     this.title = title;

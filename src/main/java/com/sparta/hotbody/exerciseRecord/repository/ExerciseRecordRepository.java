f171551ceb2c8095fa7c55f724536ca4c4af2374
SeokJin <tae993136@gmail.com>
2023-02-27 11:50:32 +0900

    Feat : 운동기록 페이징

--- src/main/java/com/sparta/hotbody/exerciseRecord/repository/ExerciseRecordRepository.java
+++ src/main/java/com/sparta/hotbody/exerciseRecord/repository/ExerciseRecordRepository.java
@@ -1,9 +1,13 @@
 package com.sparta.hotbody.exerciseRecord.repository;
 
 import com.sparta.hotbody.exerciseRecord.entity.ExerciseRecord;
+import com.sparta.hotbody.report.entity.PostReportHistory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
 public interface ExerciseRecordRepository extends JpaRepository<ExerciseRecord, Long> {
 
+  Page<ExerciseRecord> findAll(Pageable pageable);
 }

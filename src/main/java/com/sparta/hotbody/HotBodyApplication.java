9c31a1d054edf3e31be9091aefd035860b9d8bb1
sieun-Woo <116184724+sieun-Woo@users.noreply.github.com>
2023-03-04 00:22:14 +0900

    fix : 배포

--- src/main/java/com/sparta/hotbody/HotBodyApplication.java
+++ src/main/java/com/sparta/hotbody/HotBodyApplication.java
@@ -11,7 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class HotBodyApplication {
 
 
-  
+
   public static void main(String[] args) {
     SpringApplication.run(HotBodyApplication.class, args);
   }

39eaae6d3cc3bf7750fb247608c81ac6899569f1
sieun-Woo <116184724+sieun-Woo@users.noreply.github.com>
2023-03-03 23:54:43 +0900

    fix : aws 배포 수정

--- src/main/java/com/sparta/hotbody/HotBodyApplication.java
+++ src/main/java/com/sparta/hotbody/HotBodyApplication.java
@@ -10,6 +10,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableBatchProcessing
 public class HotBodyApplication {
 
+
   
   public static void main(String[] args) {
     SpringApplication.run(HotBodyApplication.class, args);

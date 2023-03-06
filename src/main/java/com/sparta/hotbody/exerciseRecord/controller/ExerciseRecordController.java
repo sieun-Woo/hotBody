8a2c2c43c9f87bb03a2d8835f6deaff9367262d7
SeokJin <tae993136@gmail.com>
2023-03-06 16:54:02 +0900

    Feat: record,report PreAuthorize적용

--- src/main/java/com/sparta/hotbody/exerciseRecord/controller/ExerciseRecordController.java
+++ src/main/java/com/sparta/hotbody/exerciseRecord/controller/ExerciseRecordController.java
@@ -8,6 +8,7 @@ import com.sparta.hotbody.user.service.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -28,6 +29,7 @@ public class ExerciseRecordController {
 
   // 운동 기록
   @PostMapping("/record")
+  @PreAuthorize("hasanyRole('USER', 'TRAINER', 'ADMIN', 'REPORTED', 'REPORTED_TRAINER')")
   public ExerciseRecordResponseDto createExerciseRecord(
       @RequestBody ExerciseRecordRequestDto exerciseRecordRequestDto,
       @AuthenticationPrincipal UserDetailsImpl userDetails) {
@@ -35,6 +37,7 @@ public class ExerciseRecordController {
   }
 
   @GetMapping("/records")
+  @PreAuthorize("hasanyRole('USER', 'TRAINER', 'ADMIN', 'REPORTED', 'REPORTED_TRAINER')")
   public Page<ExerciseRecordResponseDto> getAllExerciseRecords(
       @RequestParam("page") int page,
       @RequestParam("size") int size,
@@ -46,6 +49,7 @@ public class ExerciseRecordController {
 
   // 운동 기록 조회
   @GetMapping("/records/{recordId}")
+  @PreAuthorize("hasanyRole('USER', 'TRAINER', 'ADMIN', 'REPORTED', 'REPORTED_TRAINER')")
   public ExerciseRecordResponseDto getExerciseRecordById(@PathVariable Long recordId,
       @AuthenticationPrincipal UserDetailsImpl userDetails) {
     return exerciseRecordService.getExerciseRecordById(recordId, userDetails);
@@ -53,6 +57,7 @@ public class ExerciseRecordController {
 
   // 운동 기록 수정
   @PatchMapping("/records/{recordId}")
+  @PreAuthorize("hasanyRole('USER', 'TRAINER', 'ADMIN', 'REPORTED', 'REPORTED_TRAINER')")
   public ExerciseRecordResponseDto updateExercise(@PathVariable Long recordId,
       @RequestBody ExerciseRecordRequestDto exerciseRecordRequestDto,
       @AuthenticationPrincipal UserDetailsImpl userDetails) {
@@ -62,6 +67,7 @@ public class ExerciseRecordController {
 
   // 운동 기록 삭제
   @DeleteMapping("/records/{recordId}")
+  @PreAuthorize("hasanyRole('USER', 'TRAINER', 'ADMIN', 'REPORTED', 'REPORTED_TRAINER')")
   public ResponseEntity<String> deleteExerciseRecord(@PathVariable Long recordId,
       @AuthenticationPrincipal UserDetailsImpl userDetails) {
     return exerciseRecordService.deleteExerciseRecord(recordId, userDetails);

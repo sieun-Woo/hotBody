0e220e6b912212e27efb71963238104ccfce7ec7
sieun-Woo <116184724+sieun-Woo@users.noreply.github.com>
2023-03-08 03:20:39 +0900

    fix : presigned url

--- src/main/java/com/sparta/hotbody/upload/controller/UploadController.java
+++ src/main/java/com/sparta/hotbody/upload/controller/UploadController.java
@@ -23,43 +23,43 @@ public class UploadController {
   private final ImageRepository imageRepository;
   private final UploadService uploadService;
 
-  @PreAuthorize("hasAnyRole('USER', 'ADMIN', 'TRAINER')")
-  @GetMapping("/upload")
-  public String newFile() {
-    return "upload-form";
-  }
-
+//  @PreAuthorize("hasAnyRole('USER', 'ADMIN', 'TRAINER')")
+//  @GetMapping("/upload")
+//  public String newFile() {
+//    return "upload-form";
+//  }
 
   @GetMapping("/image")
-  public String putImage() {
-    return uploadService.putImage();
+  public String putImage(@RequestPart MultipartFile multipartFile) {
+    return uploadService.putImage(multipartFile);
   }
 
+//  @PreAuthorize("hasAnyRole('USER', 'ADMIN', 'TRAINER')")
+//  @PostMapping("/upload")
+//  public String saveFile(
+//          @RequestParam MultipartFile file, RedirectAttributes redirectAttributes) throws IOException {
+//
+//    Image image = uploadService.storeFile(file);
+//    redirectAttributes.addAttribute("itemId", image.getId());
+//
+//    return "redirect:/api/upload/{itemId}";
+//  }
 
-  @PreAuthorize("hasAnyRole('USER', 'ADMIN', 'TRAINER')")
-  @PostMapping("/upload")
-  public String saveFile(
-      @RequestParam MultipartFile file, RedirectAttributes redirectAttributes) throws IOException {
+//  @PreAuthorize("hasAnyRole('USER', 'ADMIN', 'TRAINER')")
+//  @GetMapping("/upload/{id}")
+//  public String items(@PathVariable Long id, Model model) {
+//    Image image = imageRepository.findById(id).get();
+//    model.addAttribute("item", image);
+//    return "item-view";
+//  }
 
-    Image image = uploadService.storeFile(file);
-    redirectAttributes.addAttribute("itemId", image.getId());
+//  // 이미지 조회
+//  @PreAuthorize("hasAnyRole('USER', 'ADMIN', 'TRAINER')")
+//  @ResponseBody
+//  @GetMapping("/image/{filename}")
+//  public Resource downloadImage(@PathVariable String filename) throws MalformedURLException {
+//    return uploadService.viewImage(filename);
+//  }
+//}
 
-    return "redirect:/api/upload/{itemId}";
-  }
-
-  @PreAuthorize("hasAnyRole('USER', 'ADMIN', 'TRAINER')")
-  @GetMapping("/upload/{id}")
-  public String items(@PathVariable Long id, Model model) {
-    Image image = imageRepository.findById(id).get();
-    model.addAttribute("item", image);
-    return "item-view";
-  }
-
-  // 이미지 조회
-  @PreAuthorize("hasAnyRole('USER', 'ADMIN', 'TRAINER')")
-  @ResponseBody
-  @GetMapping("/image/{filename}")
-  public Resource downloadImage(@PathVariable String filename) throws MalformedURLException {
-    return uploadService.viewImage(filename);
-  }
 }
